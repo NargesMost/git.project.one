5515e26cd477d3106b35515919614150b4540890
narges <nargesmostafazade12@gmail.com>
2024-04-04 16:46:03 +0330

    Merge branch 'dev'

--- index.cpp
+++ index.cpp
@@ -4,11 +4,8 @@ using namespace std;
 
 int main(){
     cout << "hello world\n";
-<<<<<<< HEAD
     cout << "my name is narges\n";
     cout << "most\n";
-=======
     cout << "my major is ce\n";
->>>>>>> dev
 
 }
